4ff5ce0f6543cb08a96ec8f41a17e3c01c9e27ea
seyhdavut <tekinseyhdavut@gmail.com>
2026-08-18 16:13:19 +0300

    feat: scroll to top on route change
    Implement auto-scroll to the top of the page when navigating between routes to improve user experience.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 
 export default function Layout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="flex flex-col min-h-screen font-sans bg-slate-50">
       <Header />
